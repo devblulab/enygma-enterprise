10c0e7257379ab6129b8c02ab1e164f396698c0f
devblulab <116671864+devblulab@users.noreply.github.com>
2023-09-13 23:27:19 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { AutenticacaoProvider } from '@/data/contexts/AutenticacaoContext';
 import { MantineProvider } from '@mantine/core';
 import Particles from '@/components/landing/particles';
-import { GlobalStyles } from './GlobalStyle.js';
-import { lightTheme, darkTheme } from './themes.js';
-import '@/styles/globals.css';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import '@/styles/globals.css'
+
+// Defina os temas aqui
+const lightTheme = {
+  backgroundColor: 'white',
+  textColor: 'black',
+};
+
+const darkTheme = {
+  backgroundColor: 'black',
+  textColor: 'white',
+};
+
+// Defina os estilos globais aqui
+const GlobalStyles = createGlobalStyle`
+body {
+  background-color: ${props => props.theme.backgroundColor};
+  color: ${props => props.theme.textColor};
+}
+`;
 
 function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState('light');
@@ -18,39 +36,35 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <MantineProvider>
-        <AutenticacaoProvider>
-          <div style={{ position: 'relative' }}>
-            <button 
-              onClick={toggleTheme} 
-              style={{ 
-                position: 'fixed', // Define o botão como fixo
-                top: '110px', 
-                right: '50px',
-                border: '1px solid #ccc', 
-             
-                borderRadius: '4px', 
-                padding: '10px', 
-                cursor: 'pointer',
-                zIndex: 1000, // Para garantir que o botão fique acima de outros elementos
-              }}
-            >
-              {theme === 'light' ? <FaSun style={{ fontSize: '1em' }} /> : <FaMoon style={{ fontSize: '1em' }} />} 
-              
-            </button>
-          </div>
-          <Component {...pageProps} />
-          <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
-        </AutenticacaoProvider>
-      </MantineProvider>
-    </ThemeProvider>
+    <GlobalStyles />
+    <MantineProvider>
+      <AutenticacaoProvider>
+        
+        <div style={{ position: 'relative' }}>
+          <button 
+            onClick={toggleTheme} 
+            style={{ 
+              position: 'fixed', // Define o botão como fixo
+              top: '90px', 
+              right: '50px',
+              border: '1px solid #cc32c', 
+              background: 'linear-gradient(45deg, #807770 29%, #002340 90%)',
+              borderRadius: '4px', 
+              padding: '10px', 
+              cursor: 'pointer',
+              zIndex: 1000, // Para garantir que o botão fique acima de outros elementos
+            }}
+          >
+            {theme === 'light' ? <FaSun style={{ fontSize: '1em' }} /> : <FaMoon style={{ fontSize: '1em' }} />} 
+            
+          </button>
+        </div>
+        <Component {...pageProps} />
+        <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
+      </AutenticacaoProvider>
+    </MantineProvider>
+  </ThemeProvider>
   );
 }
 
 export default App;
-
-
-
-
-
