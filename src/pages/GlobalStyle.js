e4dcc84fa6955ab9d829008f9d80f0eb3456adf4
devblulab <116671864+devblulab@users.noreply.github.com>
2023-09-13 23:26:19 +0100

    Delete src/pages/GlobalStyle.js

--- src/pages/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-export const GlobalStyles = createGlobalStyle`
-  body {
-    background-color: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.textColor};
-  }
-`;
-
