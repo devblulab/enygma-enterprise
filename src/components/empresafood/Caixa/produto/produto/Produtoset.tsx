ce26b56c5b9aabfbd83404d96b95ce320c480e66
devblulab <116671864+devblulab@users.noreply.github.com>
2024-04-09 11:11:14 -0300

    Add files via upload

---
+++ src/components/empresafood/Caixa/produto/produto/Produtoset.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ListPost from './ListPost';
+import ItemList from './ItemList';
+import ForcarAutenticacao from "../../../../components/autenticacao/ForcarAutenticacao";
+
+
+
+
+interface Item {
+  id: string;
+  cliente: string;
+  total: number;
+  status: string;
+  nome: string;
+  quantidade: number;
+  mesa: string;
+  tipo: string;
+  concluido: boolean;
+  userId: string;
+  unidadevalor: number;
+  imagemUrl: string;
+  
+  
+ 
+  
+
+}
+
+const Produtoset: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (newItem: Item) => {
+    setItems([...items, newItem]);
+  };
+
+  return (
+    <>
+    <ForcarAutenticacao>
+    
+      <ListPost setItems={(items) => addItem} />
+      <ItemList items={items} />
+      </ForcarAutenticacao>
+    </>
+  );
+};
+
+export default Produtoset;
