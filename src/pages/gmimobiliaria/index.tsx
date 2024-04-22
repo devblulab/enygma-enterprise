d9a48c387e8cc3d677439483454790914a6dca64
devblulab <116671864+devblulab@users.noreply.github.com>
2024-04-22 08:32:36 -0300

    Add files via upload

---
+++ src/pages/gmimobiliaria/index.tsx
@@ -0,0 +1,23 @@
+
+
+
+import React from 'react';
+import Produto from '@/components/enterprises/imobiliaria/Catalago/Landpage/homelandpage/Landpage';
+
+
+
+
+
+const PageProduto: React.FC = () => {
+  return (
+    <>
+    
+      
+      <Produto />
+     
+     
+    </>
+  );
+};
+
+export default PageProduto;
