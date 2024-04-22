153e446040aee1855dd82b0868fdfa9371747d83
devblulab <116671864+devblulab@users.noreply.github.com>
2024-04-22 07:46:55 -0300

    Update Inicio.tsx

--- src/components/enterprises/imobiliaria/Catalago/Landpage/homelandpage/comum/Inicio.tsx
+++ src/components/enterprises/imobiliaria/Catalago/Landpage/homelandpage/comum/Inicio.tsx
@@ -15,21 +15,31 @@ export default function Logo() {
     'text-2xl': isMobile,
   });
 
+  const handleWhatsAppClick = () => {
+    // Número de telefone para enviar a mensagem via WhatsApp
+    const phoneNumber = '5548996421279';
+    // Mensagem a ser enviada
+    const message = 'Olá, estou interessado(a) em seus serviços.';
+    // URL do link do WhatsApp
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    // Redireciona para o link do WhatsApp
+    window.open(whatsappURL, '_blank');
+  };
+
   return (
     <div className={enygmaClasses}>
-      <div className="flex items-center gap-1"> {/* Contêiner flexível para alinhar horizontalmente */}
+      <div className="flex items-center gap-1"> 
         <span className="font-black">GM</span>
         <span className="text-zinc-400 font-thin">Agente Imobiliário</span>
       </div>
       <span className="font-black">Seu sonhos aqui!!</span>
-      <Link href="/enterprises" passHref> {/* Substitua "/sua-rota-de-login-aqui" pela rota desejada */}
-        <MenuItem className="bg-gradient-to-r  from-indigo-600 to-cyan-600">
-          <div className="flex items-center gap-2">
-            <IconBrandGoogle size={12} />
-            <span>Entre em contato</span>
-          </div>
-        </MenuItem>
-      </Link>
+    
+      <MenuItem className="bg-gradient-to-r  from-indigo-600 to-cyan-600" onClick={handleWhatsAppClick}>
+        <div className="flex items-center gap-2">
+          <IconBrandGoogle size={12} />
+          <span>Entre em contato</span>
+        </div>
+      </MenuItem>
     </div>
   );
 }
