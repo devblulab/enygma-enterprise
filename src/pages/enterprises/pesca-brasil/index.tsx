6b82b2c75b5ec943c415f2eb2d658c9c33134e47
devblulab <116671864+devblulab@users.noreply.github.com>
2025-01-09 19:50:24 -0300

    Add files via upload

---
+++ src/pages/enterprises/pesca-brasil/index.tsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import Pesca from '../../../components/enterprises/pesca/landpesca';
+import Rodape from '../../../components/inventario/Footer';
+
+
+
+
+const PageFood: React.FC = () => {
+  return (
+    <>
+ 
+      
+      <Pesca />
+      <Rodape />
+   
+    </>
+  );
+};
+
+export default PageFood;
