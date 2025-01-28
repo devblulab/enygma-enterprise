44f1a77070187cfc65ca8174e42824054e00db29
devblulab <116671864+devblulab@users.noreply.github.com>
2025-01-28 15:55:37 -0300

    Update index.tsx

--- src/pages/beto/localizar/index.tsx
+++ src/pages/beto/localizar/index.tsx
@@ -1,23 +1,61 @@
+import React, { useState } from 'react';
 
-
-
-import React from 'react';
-import Produto from '@/components/enterprises/betodespa/requerimento/Itemlist';
-
+import ItemList from '@/components/enterprises/requerimento/ItemList';
 
 
 
 
-const PageProduto: React.FC = () => {
+
+interface Item {
+  id:string,
+    cliente: string,
+    total: number;
+    status: string,
+    quantidade: number;
+    imagemUrls: string[],
+    concluido: false,
+    placa: string,
+    renavam: string,
+    crv: string,
+    valordevenda: number;
+   
+    localizacao: string,
+
+    nomevendedor: string,
+    cpfvendedor: string,
+    enderecovendedor: string,
+    complementovendedor: string,
+    municipiovendedor: string,
+    emailvendedor: string,
+
+    nomecomprador: string,
+    cpfcomprador: string,
+    enderecocomprador: string,
+    complementocomprador: string,
+    municipiocomprador: string,
+    emailcomprador: string,
+   celtelcomprador: string,
+ 
+  
+
+}
+
+const Produtoset: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (newItem: Item) => {
+    setItems([...items, newItem]);
+  };
+
   return (
     <>
-
-     
-      <Produto />
+  
+    
       
+      <ItemList items={items} />
      
     </>
   );
 };
 
-export default PageProduto;
+export default Produtoset;
