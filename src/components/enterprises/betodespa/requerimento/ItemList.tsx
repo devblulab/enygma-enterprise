06ec30518dd582241a4284d8ae5eb0674c04ee40
devblulab <116671864+devblulab@users.noreply.github.com>
2025-02-14 16:08:58 -0300

    Update ItemList.tsx

--- src/components/enterprises/betodespa/requerimento/ItemList.tsx
+++ src/components/enterprises/betodespa/requerimento/ItemList.tsx
@@ -368,10 +368,12 @@ sendWhatsApp(pdfURL);
                   <Typography className={classes.sectionTitle}>Identificação do Comprador</Typography>
                   <Typography className={classes.field}><strong>Nome:</strong> {item.nomecomprador}</Typography>
                   <Typography className={classes.field}><strong>CPF/CNPJ:</strong> {item.cpfcomprador}</Typography>
-                  <Typography className={classes.field}><strong>Endereço:</strong> {item.enderecocomprador}</Typography>
-                  <Typography className={classes.field}><strong>Complemento:</strong> {item.complementocomprador}</Typography>
+                   <Typography className={classes.field}><strong>CEP:</strong> {item.cepcomprador}</Typography>
+                 
+                  <Typography className={classes.field}><strong>Eestado:</strong> {item.complementocomprador}</Typography>
                   <Typography className={classes.field}><strong>Município:</strong> {item.municipiocomprador}</Typography>
-                  <Typography className={classes.field}><strong>CEP:</strong> {item.cepcomprador}</Typography>
+                  <Typography className={classes.field}><strong>BAIRRO:</strong> {item.bairrocomprador}</Typography>
+                   <Typography className={classes.field}><strong>Endereço:</strong> {item.enderecocomprador}</Typography>
                   <Typography className={classes.field}><strong>E-mail:</strong> {item.emailcomprador}</Typography>
                   <Typography className={classes.field}><strong>CEL/TEL:</strong> {item.celtelcomprador}</Typography>
 
