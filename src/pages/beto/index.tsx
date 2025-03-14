b829fbaeef3b03d95ff5628bc36d92c5bc5210aa
devblulab <116671864+devblulab@users.noreply.github.com>
2025-03-14 15:43:24 -0300

    Update index.tsx

--- src/pages/beto/index.tsx
+++ src/pages/beto/index.tsx
@@ -112,7 +112,7 @@ const BotoesNavegacao = () => {
           { href: '/beto/requerimento', icon: <FaStore />, label: 'Intenção de Venda Loja' },
           { href: '/beto/requerimento/digital', icon: <FaPhone />, label: 'Intenção de Venda Digital' },
           { href: '/beto/dashboard/digital', icon: <FaChartPie />, label: 'Painel de Controle Digital' },
-          { href: '/beto/dashboard/empresas', icon: <FaChartPie />, label: 'Menu Empresas' }
+          { href: '/beto/empresas', icon: <FaChartPie />, label: 'Menu Empresas' }
         ].map((item, index) => (
           <motion.div 
             key={index} 
