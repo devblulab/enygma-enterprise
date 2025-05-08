e5fac9537bcdb6f865e93e7e9113461caa800cce
devblulab <116671864+devblulab@users.noreply.github.com>
2025-05-08 18:45:35 +0100

    Add files via upload

---
+++ src/pages/beto/anuencia/index.tsx
@@ -0,0 +1,23 @@
+
+
+
+import React from 'react';
+import Produto from '@/components/enterprises/betodespa/requerimentoanuencia/Produtoset';
+
+
+
+
+
+const PageProduto: React.FC = () => {
+  return (
+    <>
+
+     
+      <Produto />
+      
+     
+    </>
+  );
+};
+
+export default PageProduto;
