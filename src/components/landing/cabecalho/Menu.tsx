e5f706aa53c86b0ea474568a9564318d157609d8
devblulab <116671864+devblulab@users.noreply.github.com>
2025-06-12 13:05:25 -0300

    Update Menu.tsx

--- src/components/landing/cabecalho/Menu.tsx
+++ src/components/landing/cabecalho/Menu.tsx
@@ -1,19 +1,12 @@
-import AutenticacaoContext from '@/data/contexts/AutenticacaoContext';
-import { IconBrandGoogle } from '@tabler/icons-react';
-import { useContext } from 'react';
 import MenuItem from './MenuItem';
 
 export default function Menu() {
-  const { loginGoogle } = useContext(AutenticacaoContext);
-
   return (
-    <div className="flex ">
+    <div className="flex">
       <div className="flex gap-2">
-        <MenuItem onClick={loginGoogle} className="bg-gradient-to-r  from-indigo-600 to-cyan-600">
-          <div className="flex items-center gap-2">
-            <IconBrandGoogle size={12} />
-            <span>Login</span>
-          </div>
+        {/* Exemplo de botão de menu genérico (pode ser removido ou adaptado) */}
+        <MenuItem className="bg-gray-200 text-gray-800">
+          <span>Menu</span>
         </MenuItem>
       </div>
     </div>
