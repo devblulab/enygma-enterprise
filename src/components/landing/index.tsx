b085a065d9462ff4a558fb580cea393d2a36a961
devblulab <116671864+devblulab@users.noreply.github.com>
2025-06-25 21:52:50 -0300

    Update index.tsx

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -9,8 +9,7 @@ export default function Landing() {
     return (
         <Pagina externa>
             <Depoimentos/>
-            <Cabecalho />
-            <Rodape />
+
         </Pagina>
     )
 }
